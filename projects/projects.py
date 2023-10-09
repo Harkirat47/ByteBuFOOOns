7b2dfcb8f6e14731819d922e7000bc9a4c3a3694
Vynz123 <shubhayc14@gmail.com>
2023-10-08 22:25:42 -0700

    Video Plan

--- projects/projects.py
+++ projects/projects.py
@@ -21,3 +21,7 @@ def walruses():
 @app_projects.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
+
+@app_projects.route('/plans/')
+def plans():
+    return render_template("plans.html")
