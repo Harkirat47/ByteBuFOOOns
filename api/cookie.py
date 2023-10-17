f6fce75f231071b941d2da0557aaaf63203f652c
Harkirat47 <har.ki.kirat@gmail.com>
2023-10-17 10:50:47 -0700

    help

--- api/cookie.py
+++ api/cookie.py
@@ -1,94 +1,25 @@
-from flask import Blueprint, jsonify
-from flask_restful import Api, Resource, reqparse
-from __init__ import db
-from model.cookie import Cookie  # Import the Cookie model
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask_restful import Api, request, Resource # used for REST API building
+import requests  # used for testing
+import random
+from __init__ import login_manager, app, db
+from model.cookie import Cookie
 
-# Create a Blueprint for the Cookie API
-Cookie_api = Blueprint('Cookie_api', __name__, url_prefix='/api/Cookie')
+cookie_api = Blueprint('cookie', __name__, url_prefix='/api/cookie')
+# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
 
-# Create the API instance
-api = Api(Cookie_api)
 
-class CookieAPI(Resource):
-    def get(self, id):
-        # Retrieve a single cookie by its ID from the database
-        cookie = Cookie.query.get(id)
-
-        if cookie:
-            return cookie.to_dict(), 200
-        else:
-            return {"message": "Cookie not found"}, 404
-
-    def put(self, id):
-        parser = reqparse.RequestParser()
-        parser.add_argument("Cookie_name", type=str)
-        parser.add_argument("image", type=str)
-        parser.add_argument("stock", type=str)
-        parser.add_argument("price", type=str)
-        args = parser.parse_args()
-
-        try:
-            cookie = db.session.query(Cookie).get(id)
-            if cookie:
-                if args["Cookie_name"] is not None:
-                    cookie.Cookie_name = args["Cookie_name"]
-                if args["image"] is not None:
-                    cookie.image = args["image"]
-                if args["stock"] is not None:
-                    cookie.stock = args["stock"]
-                if args["price"] is not None:
-                    cookie.price = args["price"]
-                db.session.commit()
-                return cookie.to_dict(), 200
-            else:
-                return {"message": "Cookie not found"}, 404
-        except Exception as exception:
-            db.session.rollback()
-            return {"message": f"Error: {exception}"}, 500
-
-    def delete(self, id):
-        try:
-            cookie = db.session.query(Cookie).get(id)
-            if cookie:
-                db.session.delete(cookie)
-                db.session.commit()
-                return cookie.to_dict()
-            else:
-                return {"message": "Cookie not found"}, 404
-        except Exception as exception:
-            db.session.rollback()
-            return {"message": f"Error: {exception}"}, 500
-
-# Add the CookieAPI resource to the /api/Cookie/<int:id> endpoint
-api.add_resource(CookieAPI, "/<int:id>")
-
-class CookieListAPI(Resource):
-    def get(self):
-        # Retrieve all cookies from the database
-        cookies = Cookie.query.all()
-
-        # Prepare the data in JSON format
-        json_ready = [cookie.to_dict() for cookie in cookies]
-
-        # Return the JSON response
-        return jsonify(json_ready)
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("Cookie_name", required=True, type=str)
-        parser.add_argument("image", required=True, type=str)
-        parser.add_argument("stock", required=True, type=str)  # Adjust the type to int
-        parser.add_argument("price", required=True, type=str)  # Adjust the type to float
-        args = parser.parse_args()
-        cookie = Cookie(args["Cookie_name"], args["image"], args["stock"], args["price"])
-
-        try:
-            db.session.add(cookie)
-            db.session.commit()
-            return cookie.to_dict(), 201
-        except Exception as exception:
-            db.session.rollback()
-            return {"message": f"Error: {exception}"}, 500
-
-# Add the CookieListAPI resource to the /api/Cookie endpoint
-api.add_resource(CookieListAPI, "/")
+api = Api(cookie_api)
+class cookies:
+    class _getCookies(Resource):
+        def get(self):
+            cookies = db.session.query(Cookie).all()
+            return jsonify([cookie.alldetails() for cookie in cookies])
+        
+    class _getcookiedetails(Resource):
+        def get(self):
+            cookie = db.session.query(Cookie).filter(Cookie.id == int(request.args.get("id"))).first()
+            return jsonify(cookie.alldetails())
+    
+    api.add_resource(_getCookies, "/cookies")
+    api.add_resource(_getcookiedetails, "/cookiedetails")
--- model/cookie.py
+++ model/cookie.py
@@ -1,32 +1,68 @@
-from __init__ import db
+from __init__ import app, db
+from flask_login import UserMixin
+from sqlalchemy.exc import IntegrityError
+import os
+import pandas as pd
 
-class Cookie(db.Model):
-    __tablename__ = "Cookie"
-    id = db.Column(db.Integer, primary_key=True)  # Define a primary key column
-    Cookie_name = db.Column(db.String, nullable=False)
-    image = db.Column(db.String, nullable=False)
-    stock = db.Column(db.String, nullable=False)
-    price = db.Column(db.String, nullable=False)
+# Cookie model
+class Cookie(db.Model, UserMixin):
+    __tablename__ = 'cookies'
 
-    def __init__(self, Cookie_name, image, stock, price):
-        self.Cookie_name = Cookie_name
-        self.image = image
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), unique=True, nullable=False)
+    ingredients = db.Column(db.String(256), nullable=False)
+    stock = db.Column(db.String(256), nullable=True)  # Assuming stock can be nullable
+    image_name = db.Column(db.String(64), nullable=True)  # Assuming image_name can be nullable
+    price = db.Column(db.String(256), nullable=True)  # Assuming price can be nullable
+
+    def __init__(self, title, ingredients, stock, image_name, price):
+        self.title = title
+        self.ingredients = ingredients
         self.stock = stock
+        self.image_name = image_name
         self.price = price
 
-    def to_dict(self):
-        return {"Cookie_name": self.Cookie_name, "image": self.image, "stock": self.stock, "price": self.price}
+    def alldetails(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "ingredients": self.ingredients,
+            "stock": self.stock,  # Added stock
+            "image_name": self.image_name,
+            "price": self.price
+        }
 
-def init_cookies():
-    # You can keep the rest of your code as is
-    Cookie1 = Cookie(Cookie_name="cookie 1", image="image1.jpg", stock="100", price="2.00") #replace with real data
-    Cookie2 = Cookie(Cookie_name="cookie 2", image="image2.jpg", stock="100", price="2.00")
-    Cookie3 = Cookie(Cookie_name="cookie 3", image="image3.jpg", stock="100", price="2.00")
+# Favorite mode
+# Function to initialize cookies
+def initCookies():
+    with app.app_context():
+        print("Creating cookie tables")
+        db.create_all()
+        if db.session.query(Cookie).count() > 0:
+            return
 
-    db.session.add(Cookie1)
-    db.session.add(Cookie2)
-    db.session.add(Cookie3)
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(basedir, "../static/data/cookies.csv")  # Changed to use os.path.join for better compatibility
+        df = pd.read_csv(file_path)
 
-    db.session.commit()
+        for index, row in df.iterrows():
+            cookie = Cookie(
+                title=row['Title'],
+                ingredients=row['Ingredients'],
+                stock=row.get('Instructions', None),  # Added a get method to handle the possibility of the key not existing
+                image_name=row.get('Image_Name', None),
+                price=row.get('price', None)
+            )
 
-# Ensure you have imported the necessary modules and configured your database connection before running this code.
+            db.session.add(cookie)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+                print(f"Duplicate cookie or error: {cookie.title}")
+            except Exception as e:
+                db.session.rollback()
+                print(f"Error adding cookie at index {index}: {str(e)}")
+
+if __name__ == "__main__":
+    initCookies()
