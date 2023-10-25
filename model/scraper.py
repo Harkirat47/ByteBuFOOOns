6a9417f3672443eb922dd031bd7617ac561b841b
Vynz123 <shubhayc14@gmail.com>
2023-10-25 00:41:14 -0700

    MANY IMAGES

--- model/scraper.py
+++ model/scraper.py
@@ -1,12 +1,13 @@
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://cookiesandcups.com/recipe-finder/?_search=cookies")
 
+images = driver.find_elements(By.CLASS_NAME, 'wp-post-image')
 
-imgs = driver.find_element(By.XPATH, '//*[@id="post-22335"]/div/div/div[1]/div/a/img')
-url = imgs.get_attribute("src")
-print(url)
+for img in images:
+    url = img.get_attribute("src")
+    print(url)
+
 driver.quit()
