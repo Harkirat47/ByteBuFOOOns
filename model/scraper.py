7b6e25acc45e14db09cee0f5671ecee23d0a282b
Vynz123 <shubhayc14@gmail.com>
2023-10-25 00:46:46 -0700

    JSON FORMATTING AND ALT TAGs

--- model/scraper.py
+++ model/scraper.py
@@ -1,13 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import json
 
 driver = webdriver.Chrome()
 driver.get("https://cookiesandcups.com/recipe-finder/?_search=cookies")
 
+
 images = driver.find_elements(By.CLASS_NAME, 'wp-post-image')
+
+
+image_data = []
+
 
 for img in images:
     url = img.get_attribute("src")
-    print(url)
+    alt = img.get_attribute("alt")
+    image_data.append({"url": url, "alt": alt})
+
+image_data_json = json.dumps(image_data, indent=2)
+print(image_data_json)
 
 driver.quit()
+
+
+
